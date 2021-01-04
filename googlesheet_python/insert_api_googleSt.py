12c53487bbe835b9afb9c3da2b24d833fa05d996
InjaeLee-new <eij9404@naver.com>
2021-01-04 17:15:26 +0900

    구글스프레드 시트와 파이썬 연동하기

---
+++ googlesheet_python/insert_api_googleSt.py
@@ -0,0 +1,8 @@
+# py -m pip install gspread
+# py -m pip install --upgrade oauth2client
+# 일반적인 pip 설치는 먹히지 않아서 cmd에서 사용하기 위해 앞에 "py -m"을 붙여서 설치를 진행해주자.
+
+'''
+    gspread : python을 통해 구글 스프레드시트와 연동하고, 제어할 수 있게 하는 패키지
+    oauth2client : OAuth2를 통해 사용자 인증을 하기위해 설치함.
+'''
